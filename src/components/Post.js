6d51fe9386beaa49681b299d5fed8f0ffb5c14f1
Miguel Osorio <mial.osorio@gmail.com>
2019-05-25 20:50:21 +0200

    text class fixed

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,21 +1,18 @@
-import React, {Component} from 'react';
-import User from './User';
+import React, { Component } from "react";
+import User from "./User";
 
 export default class Post extends Component {
     render() {
         return (
             <div className="post">
-                <User 
-                src="https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg"
-                alt="man"
-                name="Random_Man"/>
-                <img src={this.props.src} alt={this.props.alt}></img>
-                <div className="post__name">
-                    some account
-                </div>
-                <div className="post_descr">
-                    some text
-                </div>
+                <User
+                    src="https://image.shutterstock.com/image-photo/close-portrait-smiling-handsome-man-260nw-1011569245.jpg"
+                    alt="man"
+                    name="Random_Man"
+                />
+                <img src={this.props.src} alt={this.props.alt} />
+                <div className="post__name">some account</div>
+                <div className="post__descr">some text</div>
             </div>
         );
     }
